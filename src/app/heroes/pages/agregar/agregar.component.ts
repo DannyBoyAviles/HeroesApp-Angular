3b4dac3874c3f687606b6787f84c25df0e76a511
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-02-23 14:29:09 -0600

    se eliminan los heroes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -78,4 +78,11 @@ export class AgregarComponent implements OnInit {
 
   }
 
+  borrarHeroe(){
+    this.heroesService.borrarHeroe(this.heroe.id!)
+      .subscribe( heroe => {
+          this.router.navigate(['/heroes']);
+      });    
+  }
+
 }
